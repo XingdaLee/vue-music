ddc96e4ee58ae5ae9d7e79708f6770e4a8156f27
LEGAL\lixx4 <xingda.li@lexisnexis.com>
2017-08-11 16:05:54 +0800

    jsonp的封装

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,24 @@
+import originJSONP from 'jsonp'
+// data是参数
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  // resolve成功时调用 reject失败时调用
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+// encodeURIComponent() 函数可把字符串作为 URI 组件进行编码。
+function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substring(1) : ''
+}
