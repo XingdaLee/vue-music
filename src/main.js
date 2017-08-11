7b52ebe2f09f19f1819c6419e851095b6bf7b835
LEGAL\lixx4 <xingda.li@lexisnexis.com>
2017-08-11 15:24:28 +0800

    页面骨架，eslint缩进的配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+// babel-polyfill补丁，对es6语法的转义
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import fastclick from 'fastclick'
 import router from './router'
-
+// 必须在webpack下注册别名才可以使用
+import 'common/stylus/index.styl'
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
