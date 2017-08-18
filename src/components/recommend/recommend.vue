afc9117246fe7636b76b540ee0b8beb572b4cf85
LEGAL\lixx4 <xingda.li@lexisnexis.com>
2017-08-18 17:15:21 +0800

    简单呈现歌单列表

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -14,6 +14,15 @@
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul>
+          <li class="item" v-for="item in discList" :key="item.dissid">
+            <div class="icon">
+              <img width="60" height="60" :src="item.imgurl" />
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
@@ -29,7 +38,8 @@ export default {
   },
   data() {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created() {
@@ -47,7 +57,7 @@ export default {
     _getDiscList() {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
-          debugger
+          this.discList = res.data.list
         }
       })
     }
