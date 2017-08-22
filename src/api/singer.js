31e247d6a408eb9360dff8b4560434bc2160804f
LEGAL\lixx4 <xingda.li@lexisnexis.com>
2017-08-22 17:59:36 +0800

    准备歌手页面所需要的数据

---
+++ src/api/singer.js
@@ -0,0 +1,24 @@
+import jsonp from 'common/js/jsonp'
+import { commonParams, options } from './config'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  // 花括号叫目标对象，后面的obj、obj1是源对象。对象合并是指：将源对象里面的属性添加到目标对象中去，若两者的属性名有冲突，后面的将会覆盖前面的
+  const data = Object.assign({}, commonParams, {
+    'channel': 'singer',
+    'page': 'list',
+    'key': 'all_all_all',
+    'pagesize': 100,
+    'pagenum': 1,
+    'needNewCode': 0,
+    'platform': 'yqq',
+    'g_tk': 5381,
+    'loginUin': 0,
+    'hostUin': 0,
+    'format': 'jsonp',
+    'inCharset': 'utf8',
+    'outCharset': 'utf-8',
+    'notice': 0
+  })
+  return jsonp(url, data, options)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>歌手页面</div>
+  <div class="singer">
+    歌手页面
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSinderList()
+  },
+  methods: {
+    _getSinderList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          // this._normalizeSinger(res.data.list)
+        }
+      })
+    },
+    // 重新封装数据
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      // 继承Singer类，头像的url在类里封装
+      list.forEach((item, index) => {
+        // 取热门歌手数据
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        // 判断是否已存在相同的key
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+    }
+  }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
- 
+.singer
+  position: fixed
+  top: 88px
+  bottom: 0
+  width: 100%
 </style>
